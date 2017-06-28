5b31b1534270eee61a9dfdcc299c6d718ad1a02e
Leigh <leighlawhon@gmail.com>
2017-06-28 10:35:14 -0400

    adding regex for validation; still working on test;

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,6 +2,7 @@ export class MainController {
   constructor () {
     'ngInject';
     this.calcStr = '';
+    this.noLetters = '(\[-+\]\?\[0-9\]\*\.\?\[0-9\]\*\.+\[\\/\\+\\-\\*\])+(\[-+\]\?\[0-9\]\*\.\?\[0-9\]+)'
     this.calculation = (a,b) => {
       return a + b;
     };
--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -11,4 +11,8 @@ describe('controllers', () => {
   it('should calculate a total', () => {
     expect(mainctrl.calculation(1,2)).toEqual(3);
   })
+  xit('should not accept anything other than numbers, operators, or spaces', () => {
+  //   expect(mainctrl.calcStr).not.toEqual('abc');
+  //   expect(mainctrl.calcStr).not.toEqual('&');
+  })
 });
